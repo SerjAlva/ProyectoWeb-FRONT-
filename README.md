2998593b570a9a9454ec01c1d16bbabf76940b3d
sergio alvarado esquivel <sergio-d.h.a@hotmail.com>
2018-05-11 06:06:54 -0500

    aqui esta el front v:

---
+++ README.md
@@ -0,0 +1,6 @@
+# ProyectoWeb
+Aquí se subirá el proyecto web acorde a una veterinaria llamada "Pety.mx"
+
+Colaboradores:
+
+  - Silva López David Alejandro     #15
---
+++ js/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
+    $('input#input_text, textarea#textarea2').characterCounter();
+    $('.slider').slider();
+  });
